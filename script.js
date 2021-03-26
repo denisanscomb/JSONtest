76e2d6ba1d6a4a00710a8f8180d4d928d4a1ab57
denisanscomb <denis@index.io>
2021-03-26 16:03:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let d = new Date();
-        document.body.innerHTML = "<h1>The time 1234 is " + d + "</h1>"
+        document.body.innerHTML = "<h1>The time 1234 why oh why is " + d + "</h1>"
  
 
 // does this work?
