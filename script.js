72774b75def47163674efee30003b200002242d9
denisanscomb <denis@index.io>
2021-03-26 16:16:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let d = new Date();
-        document.body.innerHTML = "<h1>The time 1234567 why oh why is " + d + "</h1>"
+let d = {
+          "firstName: "John",
+          "lastName: "Smith"
+};
+        document.body.innerHTML = "<h1>The time 1234567 why oh why is " + d.firstName + "</h1>"
  
 
 // why is this push not working?
