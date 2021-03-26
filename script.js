91ed4bfd0ae0487708f53b82be19606f262b1c7b
denisanscomb <denis@index.io>
2021-03-26 15:26:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var d = {
           "firstName: "John",
           "lastName: "Smith"
-}
-document.body.innerHTML = "<h1>fingers crossed " + d.firstName + "</h1>"
+};
+
+document.body.innerHTML = "<h1>fingers crossed " + d.firstName + "</h1>";
 
 // does this work?
 
