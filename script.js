c4784e537c9c3c035e501aa239331ebb1dc74922
denisanscomb <denis@index.io>
2021-03-26 16:18:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let d = {
           "firstName: "John",
           "lastName: "Smith"
 };
-        document.body.innerHTML = "<h1>The time 1234567 why oh why is " + d.firstName + "</h1>"
+        document.body.innerHTML = "<h1>The time 1234567 why oh why oh why is " + d.firstName + "</h1>"
  
 
-// why is this push not working?
+// does changing this update things?
 
 
