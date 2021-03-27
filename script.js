fef99a33052a3598a5a4849330a0d29c7b77e62d
denisanscomb <denis@index.io>
2021-03-27 07:36:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-
-        document.body.innerHTML = "<h1>Is it keeping the old variable?</h1>";
+let b = {
+          "firstName: "John",
+          "lastName: "Smith"
+};
+        document.body.innerHTML = "<h1>Is it keeping the old variable?" + b " does it read through?</h1>";
  
 
 // does changing this update things?
